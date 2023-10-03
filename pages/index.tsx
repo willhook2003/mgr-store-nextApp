d7e0914d0116ca06929d50a85c20d945ab39c8e8
Willy Ramirez <willhook2003@gmail.com>
2023-10-03 17:23:41 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
+import { ProductList } from '../components/products';
+
+
+const Home: NextPage = () => {
+  return (
+    <ShopLayout title={'Tienda-MGR - Home'} pageDescription={'Encuentra los mejores productos de Tienda MGR aquí'}>
+        <Typography variant='h1' component='h1'>Tienda</Typography>
+        <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+        <ProductList 
+          products={ initialData.products as any }
+        />
+    
+
+    </ShopLayout>
+  )
+}
+
+export default Home
